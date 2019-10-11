264dc09dd8840c2e07a462a58fd9d351f4439832
QuangVu11 <quangvm1998@gmail.com>
2019-10-11 17:01:39 +0700

    connect db

---
+++ config/dbconfig.php
@@ -0,0 +1,7 @@
+<?php
+$tenmaychu= "localhost";
+$tentaikhoan= "newuser";
+$pass= "password";
+$csdl= "hotel";
+$conn=mysqli_connect($tenmaychu,$tentaikhoan,$pass,$csdl) or die ("Khong ket noi duoc");
+?>
